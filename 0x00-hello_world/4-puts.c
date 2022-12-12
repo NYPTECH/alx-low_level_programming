a6e086e05d0ecbae2f07597a993c76d1bab73201
Nyptech <nspatrickie@gmail.com>
2022-12-11 22:22:26 -0800

    low level programing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/** 
- * main-Entry
- * Return;Always 0
- */
+
+/**
+ * main - prints exactly "Programming is like building a multilingual puzzle.
+ * Return: 0
+**/
 
 int main(void)
 {
-	puts("\"Programming is like building a multi lingual puzzle);
-return(0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
