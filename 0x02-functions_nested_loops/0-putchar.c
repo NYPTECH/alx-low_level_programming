7d8d4571eca9ee0f04acc14baaa01221b0c5f8d5
Nyptech <nspatrickie@gmail.com>
2022-12-13 22:18:29 -0800

    functions nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
 #include "main.h"
+
 /**
- * main - prints _putchar, fllowd by a new linees
- * Return: Alwys 0 (Success)
- */
+ * main - prints _putchar, followed by a new line.
+ * Return: 0
+**/
+
 int main(void)
 {
-	char abe[] = "_putchar";
+	unsigned int i;
+	char c[] = "_putchar";
 
-	int c;
-
-	for (c = 0; c < 8; c++)
+	for (i = 0; i <= 7; i++)
 	{
-		_putchar(abe[c]);
+		_putchar(c[i]);
 	}
+
 	_putchar('\n');
+
 	return (0);
 }
