206ebb0baa876dfc8ef1a4789d3e3274291fcf24
Nyptech <nspatrickie@gmail.com>
2022-12-30 03:23:18 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add -> check header file
+ * @a: an integer a
+ * @b: an integer b
+ *
+ * Return: alwasy success
+ */
+
+int add(int a, int b)
+{
+
+		return (a + b);
+}
